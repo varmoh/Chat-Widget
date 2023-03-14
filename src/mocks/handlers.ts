72f6532ea6c0933e4b155de8a42d211e9e5f6c1a
Sergei Rudz <sergei.rudz@we.ee>
2023-03-14 19:10:00 +0200

    feat #4 add download chat mock

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -8,6 +8,7 @@ import endChat from './responses/end-chat.json';
 import postMessage from './responses/post-message.json';
 import downloadChat from './responses/download-chat.json';
 import { RUUTER_ENDPOINTS } from '../constants';
+import axios from 'axios';
 
 const ruuterUrl = 'http://localhost:8080';
 
@@ -18,7 +19,26 @@ const handlers = [
   rest.post(`${ruuterUrl}${RUUTER_ENDPOINTS.END_CHAT}`, (_req, res, ctx) => res(ctx.json(endChat))),
   rest.post(`${ruuterUrl}${RUUTER_ENDPOINTS.GET_CHAT_BY_ID}`, (_req, res, ctx) => res(ctx.json(chatById))),
   rest.post(`${ruuterUrl}${RUUTER_ENDPOINTS.GET_NEW_MESSAGES}`, (_req, res, ctx) => res(ctx.json(messagesForChatInTimerange))),
-  rest.post(`${ruuterUrl}${RUUTER_ENDPOINTS.DOWNLOAD_CHAT}`, (_req, res, ctx) => res(ctx.json(downloadChat))),
+  rest.get(`${ruuterUrl}${RUUTER_ENDPOINTS.DOWNLOAD_CHAT}`, async (_req, res, ctx) => {
+    // TODO fix with correct mocking
+    const url = 'https://cors-anywhere.herokuapp.com/https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/pdf',
+        },
+    };
+    const pdfFile = await fetch(url, requestOptions)
+    .then((res) =>
+    res.arrayBuffer(),)
+
+  return res(
+    ctx.set('Content-Length', pdfFile.byteLength.toString()),
+    ctx.set('Content-Type', 'application/pdf'),
+    ctx.body(pdfFile),
+  )
+    
+  }),
 ];
 
 export default handlers;
