e3ed2cbdbdcbda2e1953aaf92233dda693a1e32d
Bahaa Alsharif <bhlshrf@gmail.com>
2024-05-28 13:35:17 +0300

    Add download link to feedback section (#189)

--- src/components/chat-feedback/chat-feedback.tsx
+++ src/components/chat-feedback/chat-feedback.tsx
@@ -1,21 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
 import StyledButton from "../styled-components/styled-button";
 import { useAppDispatch } from "../../store";
 import {
+  downloadChat,
   sendChatNpmRating,
   setFeedbackRatingGiven,
 } from "../../slices/chat-slice";
 import { StyledButtonType } from "../../constants";
 import useChatSelector from "../../hooks/use-chat-selector";
+import { Download, DownloadElement } from "../../hooks/use-download-file";
+import styles from "./chat-feedback.module.scss";
 
 const ChatFeedback = (): JSX.Element => {
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
   const { feedback } = useChatSelector();
-  const [selectedFeedbackButtonValue, setSelectedFeedbackButtonValue] =
-    useState<string>("");
+  const [selectedFeedbackButtonValue, setSelectedFeedbackButtonValue] = useState<string>("");
+  const downloadRef = useRef<DownloadElement>(null);
 
   const handleFeedback = (feedbackRating: string | null) => {
     if (feedbackRating === null) return;
@@ -23,6 +26,16 @@ const ChatFeedback = (): JSX.Element => {
     dispatch(sendChatNpmRating({ NpmRating: parseInt(feedbackRating ?? "1", 10) }));
     dispatch(setFeedbackRatingGiven(true));
   };
+  
+  const handleDownload = async () => {
+    const response = await dispatch(downloadChat(false));
+    if (response.meta.requestStatus === "rejected") {
+      return false;
+    }
+    downloadRef.current?.download({ title: `chat-history.pdf`, data: (response.payload as any).data });
+    return true;
+  };
+
   return (
     <ChatFeedbackStyle>
       <p className="feedback-paragraph above">
@@ -45,6 +58,12 @@ const ChatFeedback = (): JSX.Element => {
             <span>{val}</span>
           </StyledButton>
         ))}
+      </div>
+      <div className={styles.downloadContainer}>
+        <Download ref={downloadRef} />
+        <a onClick={handleDownload} className={styles.downloadLink}>
+          {t("widget.action.download-chat")}
+        </a>
       </div>
       <p className="feedback-paragraph below">{t("feedback.lowerText")}</p>
     </ChatFeedbackStyle>
