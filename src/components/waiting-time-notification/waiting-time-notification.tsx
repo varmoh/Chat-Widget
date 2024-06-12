ce9cf973f952075740c53c3d75a1e7a14d842cf3
baha-a <bhlshrf@gmail.com>
2024-06-12 16:50:05 +0200

    Fix waiting time message

--- src/components/waiting-time-notification/waiting-time-notification.tsx
+++ src/components/waiting-time-notification/waiting-time-notification.tsx
@@ -2,26 +2,19 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './waiting-time-notification.module.scss';
 import Button, { ButtonColor } from '../button/button';
-import formatTime from '../../utils/format-time';
 import WaitingTimeNotificationForm from './waiting-time-notification-form';
 import NotificationMessage from './notification-message';
-import useChatSelector from '../../hooks/use-chat-selector';
+import WaitingTimeInfoMessage from './waiting-time-info-message';
 import 'overlayscrollbars/css/OverlayScrollbars.css';
 import '../chat-content/os-custom-theme.scss';
 
 const WaitingTimeNotification: React.FC = () => {
   const { t } = useTranslation();
-  const { estimatedWaiting } = useChatSelector();
   const [showForm, setShowForm] = useState(false);
-
-  const FORMATTED_TIME = formatTime(estimatedWaiting.durationInSeconds);
 
   return (
     <div className={styles.container}>
-      <NotificationMessage showIcon={true}>
-        {t('notifications.waiting-time')}{' '}
-        {FORMATTED_TIME > 0 ? `${FORMATTED_TIME} ${t('widget.time.minutes')}` : `${FORMATTED_TIME} ${t('widget.time.minute')}`}
-      </NotificationMessage>
+      <WaitingTimeInfoMessage />
       <NotificationMessage showIcon={false}>
         {t('notifications.ask-contact-information')}
       </NotificationMessage>
