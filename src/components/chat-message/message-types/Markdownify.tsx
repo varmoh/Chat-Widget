0ac147c65231281176df359da023e013dc54d7ce
Ahmed yasser <26207361+1AhmedYasser@users.noreply.github.com>
2025-01-21 12:24:28 +0200

    Added Hyperlink Support to links (#363)

--- src/components/chat-message/message-types/Markdownify.tsx
+++ src/components/chat-message/message-types/Markdownify.tsx
@@ -5,17 +5,19 @@ interface MarkdownifyProps {
   message: string | undefined;
 }
 
-const LinkPreview: React.FC<{ href: string }> = ({ href }) => {
+const LinkPreview: React.FC<{ href: string; children: React.ReactNode }> = ({ href, children }) => {
   const [hasError, setHasError] = useState(false);
   return !hasError ? (
     <img
       src={href}
-      alt="Image Preview"
+      alt={typeof children === "string" ? children : "Preview"}
       style={{ maxWidth: "100%", height: "auto", borderRadius: "20px" }}
       onError={() => setHasError(true)}
     />
   ) : (
-    <a href={href} target="_blank" rel="noopener noreferrer">{href}</a>
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      {children}
+    </a>
   );
 };
 
