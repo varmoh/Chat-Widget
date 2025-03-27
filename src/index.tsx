a9a667dbbde68d00ca21d511967f8f5a77215631
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2025-03-27 02:02:31 +0200

    chore(404): Added Base Path

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import { store } from './store';
-import AuthCallback from './AuthCallback';
-import './i18n';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./store";
+import "./i18n";
+import "./index.scss";
+
+const getBasePath = () => {
+  const { pathname } = window.location;
+  if (!pathname || pathname === "/") return "/";
+  const basePath = pathname.split("/")[1];
+  return `/${basePath}`;
+};
+
+const baseName = getBasePath();
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={baseName}>
       <Routes>
-        <Route path='/' Component={App} />
-        <Route path='/auth/callback' Component={AuthCallback} />
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   </Provider>,
