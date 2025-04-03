3135ab7e73aa78ce21a08ea7b39e2956406aa789
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-03 14:30:33 +0300

    Revert "WIP"
    This reverts commit 8906d73fe3140d13de83abeba9c48baf8f7328ee.

--- src/components/chat-content/chat-content.tsx
+++ src/components/chat-content/chat-content.tsx
@@ -1,13 +1,13 @@
-import { AnimatePresence } from "framer-motion";
-import { useEffect, useRef } from "react";
-import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
-import ChatMessage from "../chat-message/chat-message";
-import useChatSelector from "../../hooks/use-chat-selector";
-import styles from "./chat-content.module.scss";
-import WaitingTimeNotification from "../waiting-time-notification/waiting-time-notification";
-import "overlayscrollbars/css/OverlayScrollbars.css";
-import "./os-custom-theme.scss";
-import LoadingMessage from "../chat-message/message-types/loading-message";
+import { AnimatePresence } from 'framer-motion';
+import { useEffect, useRef } from 'react';
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
+import ChatMessage from '../chat-message/chat-message';
+import useChatSelector from '../../hooks/use-chat-selector';
+import styles from './chat-content.module.scss';
+import WaitingTimeNotification from '../waiting-time-notification/waiting-time-notification';
+import 'overlayscrollbars/css/OverlayScrollbars.css';
+import './os-custom-theme.scss';
+import LoadingMessage from '../chat-message/message-types/loading-message';
 
 const ChatContent = (): JSX.Element => {
   const OSref = useRef<OverlayScrollbarsComponent>(null);
@@ -16,7 +16,7 @@ const ChatContent = (): JSX.Element => {
   useEffect(() => {
     if (OSref.current) {
       const instance = OSref.current.osInstance();
-      instance?.scroll({ y: "100%" }, 200);
+      instance?.scroll({ y: '100%' }, 200);
     }
   }, [messages, failedMessages]);
 
@@ -28,63 +28,26 @@ const ChatContent = (): JSX.Element => {
           ref={OSref}
           options={{
             overflowBehavior: {
-              x: "hidden",
+              x: 'hidden',
             },
-            scrollbars: { visibility: "auto", autoHide: "leave" },
+            scrollbars: { visibility: 'auto', autoHide: 'leave' },
           }}
         >
-          {/* TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-          TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST */}
           {messages.map((message, index) => {
-            if (message.id === "estimatedWaiting" && message.content === "hidden") return <></>;
-            if (message.id === "estimatedWaiting") return <WaitingTimeNotification key={message.authorTimestamp} />;
+            if(message.id === "estimatedWaiting" && message.content === "hidden")
+              return <></>;
+            if(message.id === "estimatedWaiting")
+              return <WaitingTimeNotification key={message.authorTimestamp} />;
 
             return (
-              <ChatMessage
+              <ChatMessage 
                 message={message}
                 key={`${message.authorTimestamp}-${message.created}-${message.id}`}
                 previousMessage={index > 0 ? messages[index - 1] : undefined}
               />
             );
           })}
-          {showLoadingMessage && <LoadingMessage />}
+          {showLoadingMessage && <LoadingMessage/>}
         </OverlayScrollbarsComponent>
       </div>
     </AnimatePresence>
