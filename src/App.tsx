b027cf75f1aac108630418841b98a7476f297a9f
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-04 09:01:39 +0300

    to do

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,7 @@ const App: FC = () => {
       window.removeEventListener("touchmove", preventDefault, false);
     }
 
-    // disableScroll();
+    disableScroll();
     // enableScroll();
   }, []);
 
