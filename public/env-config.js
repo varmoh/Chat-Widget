61cdd2851eb98dc96a9005443d6ba712ee66b797
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-07 17:10:26 +0300

    Revert "fix: env set up - REVERT LATER"
    This reverts commit 8bede22918e64a7d09814d34129f869f50fa98cb.

--- public/env-config.js
+++ public/env-config.js
@@ -1,9 +1,9 @@
 window._env_ = {
-  RUUTER_API_URL: "http://10.0.0.107:8086/backoffice",
-  NOTIFICATION_NODE_URL: "http://10.0.0.107:4040",
+  RUUTER_API_URL: "http://localhost:8086/backoffice",
+  NOTIFICATION_NODE_URL: "http://localhost:4040",
   ENVIRONMENT: "development", // 'developement | production'
   TIM_AUTHENTICATION_URL:
-    "http://10.0.0.107:8085/oauth2/authorization/tara?callback_url=http://10.0.0.107:3000/auth/callback",
+    "http://localhost:8085/oauth2/authorization/tara?callback_url=http://localhost:3000/auth/callback",
   ORGANIZATION_NAME: "TTJA",
   OFFICE_HOURS: {
     TIMEZONE: "Europe/Tallinn",
