08b8eebb2aa6fcb6ad83106f2c1e2590a6657a01
Janno Stern <jannostern@users.noreply.github.com>
2025-09-05 09:52:57 +0300

    Fix chat widget user authentication redirect

--- src/AuthCallback.tsx
+++ src/AuthCallback.tsx
@@ -1,12 +1,21 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { LOCAL_STORAGE_TARA_LOGIN_REDIRECT } from "./constants";
 
 const AuthCallback = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    navigate('/', { replace: true });
-  }, []);
+    const redirectPath = localStorage.getItem(LOCAL_STORAGE_TARA_LOGIN_REDIRECT);
+    const allowedURLPattern = /(https?:\/\/)?([\da-z.-]+)\.([a-z]{2,6})([/\w.-]+)+\/?/;
+
+    if (redirectPath && allowedURLPattern.test(redirectPath)) {
+      localStorage.removeItem(LOCAL_STORAGE_TARA_LOGIN_REDIRECT);
+      navigate(redirectPath, { replace: true });
+    } else {
+      navigate("/", { replace: true });
+    }
+  }, [navigate]);
 
   return (
     <span>processing authentication...</span>
