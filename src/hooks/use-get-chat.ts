7b45add537606eadc754f98991391ab817369380
Janno Stern <jannostern@users.noreply.github.com>
2025-10-29 09:50:48 +0200

    Remove user authentication when chat is ended

--- src/hooks/use-get-chat.ts
+++ src/hooks/use-get-chat.ts
@@ -2,19 +2,25 @@ import { useEffect, useState } from 'react';
 import { useAppDispatch } from '../store';
 import sse from '../services/sse-service';
 import useChatSelector from './use-chat-selector';
+import useAuthenticationSelector from "./use-authentication-selector";
 import { setChat } from '../slices/chat-slice';
+import { setIsNotAuthenticated } from "../slices/authentication-slice";
 import chatService from '../services/chat-service';
 
 const useGetChat = (): void => {
+  const { isAuthenticated } = useAuthenticationSelector();
   const { isChatEnded, chatId } = useChatSelector();
   const dispatch = useAppDispatch();
   const [sseUrl, setSseUrl] = useState('');
 
   useEffect(() => {
-    if (isChatEnded || !chatId){
-      setSseUrl('');
-    } else if(chatId) {
-      setSseUrl('/notifications/chat-list');
+    if (isChatEnded || !chatId) {
+      setSseUrl("");
+      if (isAuthenticated) {
+        dispatch(setIsNotAuthenticated());
+      }
+    } else if (chatId) {
+      setSseUrl("/notifications/chat-list");
     }
   }, [chatId, isChatEnded]);
 
