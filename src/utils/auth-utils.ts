a6a33cb7136086a5b7f820cc9322ea0ff637f68d
Janno Stern <jannostern@users.noreply.github.com>
2025-10-30 11:57:49 +0200

    Improve TARA authentication implementation

--- src/utils/auth-utils.ts
+++ src/utils/auth-utils.ts
@@ -1,10 +1,16 @@
+import { LOCAL_STORAGE_TARA_LOGIN_REDIRECT } from "../constants";
 import widgetService from "../services/widget-service";
 
 export function redirectToTim() {
+  localStorage.setItem(LOCAL_STORAGE_TARA_LOGIN_REDIRECT, 'true');
   window.location.assign(`${window._env_.TIM_AUTHENTICATION_URL}?callback_url=${encodeURIComponent(window.location.href)}`);
 }
 
 export async function redirectIfComeBackFromTim(callback: any) {
-  await widgetService.authenticateUser();
-  callback();
+  const taraLoginRedirect = localStorage.getItem(LOCAL_STORAGE_TARA_LOGIN_REDIRECT);
+  if (taraLoginRedirect === 'true') {
+    localStorage.removeItem(LOCAL_STORAGE_TARA_LOGIN_REDIRECT);
+    await widgetService.authenticateUser();
+    callback();
+  }
 }
