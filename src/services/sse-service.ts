51ab6d0ef9090584cbc64701af1e1be9d17b7424
Varmo <101868197+varmoh@users.noreply.github.com>
2026-09-01 13:14:09 +0300

    test2

--- src/services/sse-service.ts
+++ src/services/sse-service.ts
@@ -1,8 +1,12 @@
 const sse = <T>(url: string, onMessage: (data: T) => void): EventSource => {
-  const eventSource = new EventSource(
-    `${window._env_.NOTIFICATION_NODE_URL}/public/v1/notifications/events?chatUuid=dee9c8da-2b40-4c6a-a31e-db278b6960b1`
-  );
 
+  const eventSource = new EventSource(
+  `${window._env_.NOTIFICATION_NODE_URL}/public/v1/notifications/events?chatUuid=dee9c8da-2b40-4c6a-a31e-db278b6960b1`,
+  {
+    withCredentials: true,
+  }
+);
+ 
   eventSource.onmessage = (event: MessageEvent) => {
     const response = JSON.parse(event.data);
 
