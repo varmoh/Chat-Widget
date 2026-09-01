17742af14be7cca0b43261bb2b4ae1a6bb45ef67
Varmo <101868197+varmoh@users.noreply.github.com>
2026-09-01 13:03:45 +0300

    testing

--- src/services/sse-service.ts
+++ src/services/sse-service.ts
@@ -1,6 +1,6 @@
 const sse = <T>(url: string, onMessage: (data: T) => void): EventSource => {
   const eventSource = new EventSource(
-    `${window._env_.NOTIFICATION_NODE_URL}/sse${url}`
+    `${window._env_.NOTIFICATION_NODE_URL}/public/v1/notifications/events?chatUuid=dee9c8da-2b40-4c6a-a31e-db278b6960b1`
   );
 
   eventSource.onmessage = (event: MessageEvent) => {
